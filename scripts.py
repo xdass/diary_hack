254fb550016c8aa7749f9f3266687af8f6b33bcd
Dmitry <420600@gmail.com>
2021-03-27 18:56:02 +0500

    Добавлен метод get для поиска значения. Код приведен к pep8

--- scripts.py
+++ scripts.py
@@ -1,51 +1,57 @@
 from random import choice
 
-from datacenter.models import Schoolkid, Mark, Chastisement, Lesson, Commendation
+from datacenter.models import Schoolkid, Mark, Chastisement, Lesson, \
+    Commendation
 from django.core.exceptions import ObjectDoesNotExist
 
 
 def fix_marks(schoolkid):
-    """Исправляет оценки 2,3 на 5 у выбранного ученика"""
+    """Исправляет оценки 2,3 на 5 у выбранного ученика."""
     try:
-        user = Schoolkid.objects.filter(full_name__contains=schoolkid)
-        marks = Mark.objects.filter(schoolkid=user[0], points__in=[2, 3])
+        user = Schoolkid.objects.get(full_name__contains=schoolkid)
+        marks = Mark.objects.filter(schoolkid=user, points__in=[2, 3])
         for mark in marks:
             mark.points = 5
             mark.save()
     except Schoolkid.DoesNotExist:
-        print(f'{schoolkid} не найден в базе данных. Проверьте правильность ввода')
+        print(f'{schoolkid} не найден в базе данных. Проверьте правильность'
+              f'ввода')
     except Schoolkid.MultipleObjectsReturned:
         print(f'Найдено более 1 записи - {schoolkid}')
 
 
 def remove_chastisements(schoolkid):
-    """Удаляет все замечания у выбранного ученика"""
-    try:
-        all_chastisement = Chastisement.objects.filter(schoolkid__full_name__contains=schoolkid)
-        all_chastisement.delete()
-    except Schoolkid.DoesNotExist:
-        print(f'{schoolkid} не найден в базе данных. Проверьте правильность ввода')
+    """Удаляет все замечания у выбранного ученика."""
+    all_chastisement = Chastisement.objects.filter(
+        schoolkid__full_name__contains=schoolkid)
+    all_chastisement.delete()
 
 
 def get_commedation():
-    """Получение текста похвалы"""
+    """Получение текста похвалы."""
     with open('commedations', encoding='utf') as fh:
-        lines = fh.read().split('\n')
+        lines = fh.read().splitlines()
     return choice(lines)
 
 
 def create_commendation(schoolkid, lesson_title):
-    """Создает похвалу для выбранного ученика и предмета"""
+    """Создает похвалу для выбранного ученика и предмета."""
     try:
         user = Schoolkid.objects.get(full_name__contains=schoolkid)
-        lesson = Lesson.objects.filter(group_letter=user.group_letter, year_of_study=user.year_of_study, subject__title=lesson_title).order_by('-date').first()
-        Commendation.objects.create(text=get_commedation(), schoolkid=user, created=lesson.date, subject=lesson.subject, teacher=lesson.teacher)
+        lesson = Lesson.objects.filter(group_letter=user.group_letter,
+                                       year_of_study=user.year_of_study,
+                                       subject__title=lesson_title)\
+            .order_by('-date').first()
+        Commendation.objects.create(text=get_commedation(), schoolkid=user,
+                                    created=lesson.date,
+                                    subject=lesson.subject,
+                                    teacher=lesson.teacher)
     except ObjectDoesNotExist:
-        print(f'{schoolkid} не найден в базе данных. Проверьте правильность ввода')
+        print(f'{schoolkid} не найден в базе данных. Проверьте правильность '
+              f'ввода')
     except AttributeError:
-        print(f"{lesson_title} не найден в базе данных. Проверьте правильность ввода")
+        print(f'{lesson_title} не найден в базе данных. Проверьте '
+              f'правильность ввода')
     except Schoolkid.MultipleObjectsReturned:
-        print(f'Найдено более 1 записи - {schoolkid}. Укажите полное ФИО ученика')
-
-
-
+        print(f'Найдено более 1 записи - {schoolkid}. Укажите полное ФИО '
+              f'ученика')

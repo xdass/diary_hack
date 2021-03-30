8d8bffca844eed893de47e10b44f9ec89ecf6ec0
Dmitry <420600@gmail.com>
2021-03-30 11:34:02 +0500

    Исправлены названия переменных

--- scripts.py
+++ scripts.py
@@ -5,25 +5,25 @@ from datacenter.models import Schoolkid, Mark, Chastisement, Lesson, \
 from django.core.exceptions import ObjectDoesNotExist
 
 
-def fix_marks(schoolkid):
+def fix_marks(full_name):
     """Исправляет оценки 2,3 на 5 у выбранного ученика."""
     try:
-        user = Schoolkid.objects.get(full_name__contains=schoolkid)
-        marks = Mark.objects.filter(schoolkid=user, points__in=[2, 3])
+        schoolkid = Schoolkid.objects.get(full_name__contains=full_name)
+        marks = Mark.objects.filter(schoolkid=schoolkid, points__in=[2, 3])
         for mark in marks:
             mark.points = 5
             mark.save()
     except Schoolkid.DoesNotExist:
-        print(f'{schoolkid} не найден в базе данных. Проверьте правильность'
+        print(f'{full_name} не найден в базе данных. Проверьте правильность'
               f'ввода')
     except Schoolkid.MultipleObjectsReturned:
-        print(f'Найдено более 1 записи - {schoolkid}')
+        print(f'Найдено более 1 записи - {full_name}')
 
 
-def remove_chastisements(schoolkid):
+def remove_chastisements(full_name):
     """Удаляет все замечания у выбранного ученика."""
     all_chastisement = Chastisement.objects.filter(
-        schoolkid__full_name__contains=schoolkid)
+        schoolkid__full_name__contains=full_name)
     all_chastisement.delete()
 
 
@@ -34,24 +34,26 @@ def get_commedation():
     return choice(lines)
 
 
-def create_commendation(schoolkid, lesson_title):
+def create_commendation(full_name, lesson_title):
     """Создает похвалу для выбранного ученика и предмета."""
     try:
-        user = Schoolkid.objects.get(full_name__contains=schoolkid)
-        lesson = Lesson.objects.filter(group_letter=user.group_letter,
-                                       year_of_study=user.year_of_study,
+        schoolkid = Schoolkid.objects.get(full_name__contains=full_name)
+        lesson = Lesson.objects.filter(group_letter=schoolkid.group_letter,
+                                       year_of_study=schoolkid.year_of_study,
                                        subject__title=lesson_title)\
             .order_by('-date').first()
-        Commendation.objects.create(text=get_commedation(), schoolkid=user,
+        if not lesson:
+            print(f"{lesson_title} не найден в базе данных."
+                  f" Проверьте правильность ввода")
+            return
+        Commendation.objects.create(text=get_commedation(), schoolkid=schoolkid,
                                     created=lesson.date,
                                     subject=lesson.subject,
                                     teacher=lesson.teacher)
     except ObjectDoesNotExist:
-        print(f'{schoolkid} не найден в базе данных. Проверьте правильность '
+        print(f'{full_name} не найден в базе данных. Проверьте правильность '
               f'ввода')
-    except AttributeError:
-        print(f'{lesson_title} не найден в базе данных. Проверьте '
-              f'правильность ввода')
     except Schoolkid.MultipleObjectsReturned:
-        print(f'Найдено более 1 записи - {schoolkid}. Укажите полное ФИО '
+        print(f'Найдено более 1 записи - {full_name}. Укажите полное ФИО '
               f'ученика')
+
